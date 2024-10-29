46ab51ce4a92d831420b65f8d225c0cbc8556377
Darneur <20408795+Darneur@users.noreply.github.com>
2024-10-29 23:18:07 +0900

    Dynamic resize implemented

---
+++ OpenCV_Integration_ja.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja" sourcelanguage="en">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="28"/>
+        <source>File Path</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="38"/>
+        <source>Start</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="59"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="71"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="main.cpp" line="19"/>
+        <source>This is a test</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
--- main.cpp
+++ main.cpp
@@ -2,12 +2,22 @@
 
 #include <QApplication>
 #include <QDir>
+#include <QTranslator>
+
+#include <QPushButton>
 
 int main(int argc, char *argv[])
 {
     // Sets the current directory to make resources easier to import
     QDir::setCurrent("../..");
     QApplication a(argc, argv);
+    QTranslator translator;
+    Q_UNUSED(translator.load(":/OpenCV_Integration_en_US"));
+    a.installTranslator(&translator);
+
+    // For translating
+    QObject::tr("This is a test");
+
     MainWindow w;
     w.show();
     return a.exec();
