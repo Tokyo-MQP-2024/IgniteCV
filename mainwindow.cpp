eb02472ac55ad89fd1d2867bf789776a5729a241
Darneur <20408795+Darneur@users.noreply.github.com>
2024-11-18 21:26:20 +0900

    removed comments

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -197,101 +197,6 @@ void MainWindow::on_pushButton_4_clicked() {
     ui->pushButton_4->setDisabled(false);
 }
 
-
-
-// // Open Image for width calculation
-// void MainWindow::on_pushButton_6_clicked() {
-//     QString fileName = QFileDialog::getOpenFileName(this, "Open A File", "C://");
-//     ui->label_5->setText(fileName);
-//     cv::Mat image = cv::imread(fileName.toStdString());
-//     // Display to graphics window
-//     QImage toDisplay = matToQImage(image);
-//     ui->graphicsView_2->setScene(new QGraphicsScene(this));
-//     ui->graphicsView_2->scene()->addPixmap(QPixmap::fromImage(toDisplay));
-//     ui->graphicsView_2->fitInView(ui->graphicsView_2->scene()->sceneRect(), Qt::KeepAspectRatioByExpanding);
-// }
-
-// // Run width calculation on image
-// void MainWindow::on_pushButton_7_clicked() {
-//     QString fileName = ui->label_5->text();
-//     cv::Mat image = cv::imread(fileName.toStdString());
-
-//     // Apply threshold from slider
-//     int sliderValue = ui->horizontalSlider->value();
-//     int threshType = 0;
-
-//     // Manually check radio buttons
-//     if(ui->radioButton_binary->isChecked()) {
-//         threshType = 0;
-//     } else if(ui->radioButton_bininverted->isChecked()) {
-//         threshType = 1;
-//     } else if(ui->radioButton_threshtrunc->isChecked()) {
-//         threshType = 2;
-//     } else if(ui->radioButton_thresh0->isChecked()) {
-//         threshType = 3;
-//     } else if(ui->radioButton_thresh0inv->isChecked()) {
-//         threshType = 4;
-//     } else {
-//         qErrnoWarning("ERROR: TYPE NOT DEFINED");
-//     }
-
-//     cv::threshold(image, image, sliderValue, 255, threshType);
-
-//     //QImage screen(ui->graphicsView_2->viewport()->size(), QImage::Format_RGB32);
-//     //QPainter painter(&screen);
-//     //ui->graphicsView_2->render(&painter);
-//     //cv::Mat image = QImageToCvMat(screen);
-
-//     // Modify image in place
-//     imageWidthOverlay(image);
-//     //cv::Rect2d r = cv::selectROI(image);
-//     //cv::Mat imCrop = image(r);
-//     //cv::imshow("Result", imCrop);
-//     cv::imshow("Result", image);
-
-//     // Display to graphics window
-//     // QImage toDisplay = matToQImage(image);
-//     // ui->graphicsView_2->setScene(new QGraphicsScene(this));
-//     // ui->graphicsView_2->scene()->addPixmap(QPixmap::fromImage(toDisplay));
-//     // ui->graphicsView_2->fitInView(ui->graphicsView_2->scene()->sceneRect(), Qt::KeepAspectRatio);
-
-// }
-
-// // When slider changes, edit image in display with appropriate thresholding
-// void MainWindow::on_horizontalSlider_sliderMoved(int position) {
-//     QString label = ui->label_5->text();
-//     if(label == "")
-//         return;
-//     cv::Mat image = cv::imread(label.toStdString());
-//     cv::Mat grey, output;
-//     // Convert to grey
-//     cv::cvtColor(image, grey, cv::COLOR_BGR2GRAY);
-//     int threshType = 0;
-
-//     // Manually check radio buttons
-//     if(ui->radioButton_binary->isChecked()) {
-//         threshType = 0;
-//     } else if(ui->radioButton_bininverted->isChecked()) {
-//         threshType = 1;
-//     } else if(ui->radioButton_threshtrunc->isChecked()) {
-//         threshType = 2;
-//     } else if(ui->radioButton_thresh0->isChecked()) {
-//         threshType = 3;
-//     } else if(ui->radioButton_thresh0inv->isChecked()) {
-//         threshType = 4;
-//     } else {
-//         qErrnoWarning("ERROR: TYPE NOT DEFINED");
-//     }
-
-//     // Threshold type hardcoded for now
-//     cv::threshold(grey, output, position, 255, threshType);
-//     QImage toDisplay = matToQImage(output);
-//     ui->graphicsView_2->scene()->clear();
-//     ui->graphicsView_2->scene()->addPixmap(QPixmap::fromImage(toDisplay));
-//     ui->graphicsView_2->fitInView(ui->graphicsView_2->scene()->sceneRect(), Qt::KeepAspectRatio);
-
-// }
-
 // button for running video processing program
 void MainWindow::on_ProcessVideoButton_clicked()
 {
@@ -319,26 +224,6 @@ void MainWindow::on_VideoSelectButton_clicked()
         ui->VideoLabel->setText("File must be .mp4");
     }
 }
-
-// // Checkbox for ROI
-// void MainWindow::on_checkBox_checkStateChanged(const Qt::CheckState &arg1) {
-//     if(arg1 == Qt::Checked) {
-//         for(int i = 0; i < ui->horizontalLayout_roi->count(); ++i) {
-//             QWidget* widget = ui->horizontalLayout_roi->itemAt(i)->widget();
-//             if(QSpinBox* spinBox = qobject_cast<QSpinBox*>(widget)) {
-//                 spinBox->setEnabled(true);
-//             }
-//         }
-//     } else {
-//         for(int i = 0; i < ui->horizontalLayout_roi->count(); ++i) {
-//             QWidget* widget = ui->horizontalLayout_roi->itemAt(i)->widget();
-//             if(QSpinBox* spinBox = qobject_cast<QSpinBox*>(widget)) {
-//                 spinBox->setEnabled(false);
-//             }
-//         }
-//     }
-// }
-
 
 void MainWindow::on_CancelButton_clicked()
 {
